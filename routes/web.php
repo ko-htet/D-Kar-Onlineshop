b7634c27bf8c1695f805dbe71203b863966fda5b
Han Min Htet <mghanmhtet91@gmail.com>
2020-11-23 22:08:09 +0630

    web update

--- routes/web.php
+++ routes/web.php
@@ -2,24 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-// using frontend asset
-// Route::get('/', 'MainController@welcome')->name('homepage');
-// Route::get('testingone', 'MainController@testing')->name('testingpage');
-// Route::get('about', 'MainController@about')->name('aboutpage');
-// Route::get('contact', 'MainController@contact')->name('contactpage');
-
 Route::middleware('role:admin')->group(function(){
-    // CRUD process (item management)
     Route::resource('brand', 'BrandController');
     Route::resource('category', 'CategoryController');
     Route::resource('subcategory', 'SubcategoryController');
@@ -27,7 +10,6 @@ Route::middleware('role:admin')->group(function(){
     Route::post('filter', 'ItemController@filterCategory')->name('filterCategory');
 });
 
-// frontend with items
 Route::get('/', 'FrontendController@home')->name('mainpage');
 Route::get('signin', 'FrontendController@signin')->name('signinpage');
 Route::get('signup', 'FrontendController@signup')->name('signuppage');
@@ -48,5 +30,3 @@ Route::post('delivery/{id}', 'OrderController@delivery')->name('order.delivery')
 Route::resource('user', 'UserController');
 
 Auth::routes(['register' => false]);
-
-Route::get('/home', 'HomeController@index')->name('home');
